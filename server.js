b06ccebd4723c87927a74b5457eeb9a9c6830c34
fcbazanti <tpriklenk5@gmail.com>
2025-10-21 07:01:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import nodemailer from 'nodemailer';
 import { PDFDocument, StandardFonts } from 'pdf-lib';
 
 dotenv.config();
-
+import registerStripeWebhook from "./stripe-webhook.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
